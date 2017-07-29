fae15890540f1aa5b6183043006d829bd1c73719
Jigar Gosalia <jgosalia@paypal.com>
2017-07-28 18:36:19 -0700

    LAZY: update BindService

--- BindService/app/src/main/java/edu/someschool/android/bindservice/MainActivity.java
+++ BindService/app/src/main/java/edu/someschool/android/bindservice/MainActivity.java
@@ -16,9 +16,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-
-
-
 public class MainActivity extends Activity implements View.OnClickListener,
 		ServiceConnection {
 	private Button btnStart, btnStop, btnBind, btnUnbind, btnUpby1, btnUpby10;
